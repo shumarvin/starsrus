8575ec6311e9bc73d51416f3a22c50ae6a3fc2d9
Marvin Shu <marvinshu@umail.ucsb.edu>
2017-11-15 16:22:35 -0800

    basic trader interface done

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -25,8 +25,6 @@ public class UserInterface
 		System.out.println("Welcome to Stars'R'Us!");
 		System.out.println("*************************");
 
-		//dbAdapter.connect();
-		//dbAdapter.close();
 
 		//start login process here
 		showLoginChoices();
@@ -39,6 +37,7 @@ public class UserInterface
 			{
 				System.out.println("Please choose one of the 4 options above.");
 				showLoginChoices();
+				System.out.print("Input: ");
 				choice = reader.nextInt();
 			}
 
@@ -103,10 +102,7 @@ public class UserInterface
 			customerPassword = new String(customerPassCharArr);
 			account = dbAdapter.queryAccount(0, customerUsername, customerPassword);
 		}
-		
-		System.out.println("Welcome " + account.getFirstName() + " " +
-				account.getLastName() + "!");
-		
+		showTraderInterface();
 	}
 	//manager login user interface
 	private void doManagerLogin()
@@ -126,6 +122,105 @@ public class UserInterface
 	private void doCreateAccount()
 	{
 		System.out.println("create account");
+	}
+	//trader user interface
+	private void showTraderInterface()
+	{
+		System.out.println();
+		System.out.println("Welcome " + account.getFirstName() + " " +
+				account.getLastName() + "!");
+		System.out.println();
+
+		System.out.println("What would you like to do today?");
+		System.out.println();
+
+		showTraderInterfaceChoices();
+		try
+		{
+			System.out.print("Input: ");
+			int choice = reader.nextInt();
+			//handle invalid input
+			while(choice < 1 || choice > 9)
+			{
+				System.out.println();
+				System.out.println("Please choose one of the 9 options above.");
+				showTraderInterfaceChoices();
+				System.out.print("Input: ");
+				choice = reader.nextInt();
+			}
+
+			//switch on choice
+			switch(choice)
+			{
+				case 1: showDeposit();
+						break;
+				case 2: showWithdraw();
+						break;
+				case 3: showBuy();
+						break;
+				case 4: showSell();
+						break;
+				case 5: showMarketBalance();
+						break;
+				case 6: showStockTransactions();
+						break;
+				case 7: showCurrentStockPrice();
+						break;
+				case 8: showMovieInfo();
+						break;
+				default: quit();
+			}
+		}
+		catch(InputMismatchException exception)
+		{
+			System.out.println("Error! Invalid Input!");
+		}
+
+	}
+	//trader user interface choices
+	private void showTraderInterfaceChoices()
+	{
+		System.out.println("1. Deposit into Market Account");
+		System.out.println("2. Withdraw from Market Account");
+		System.out.println("3. Buy Stocks");
+		System.out.println("4. Sell Stocks");
+		System.out.println("5. Show Market Account balance");
+		System.out.println("6. Show Stock Transaction History");
+		System.out.println("7. List Current Stock Price");
+		System.out.println("8. List Movie Information");
+		System.out.println("9. Log out");
+	}
+	private void showDeposit()
+	{
+		System.out.println("show deposit");
+	}
+	private void showWithdraw()
+	{
+		System.out.println("show withdraw");
+	}
+	private void showBuy()
+	{
+		System.out.println("show buy");
+	}
+	private void showSell()
+	{
+		System.out.println("show sell");
+	}
+	private void showMarketBalance()
+	{
+		System.out.println("show market balance");
+	}
+	private void showStockTransactions()
+	{
+		System.out.println("show stock transactions");
+	}
+	private void showCurrentStockPrice()
+	{
+		System.out.println("show current stock price");
+	}
+	private void showMovieInfo()
+	{
+		System.out.println("show movie info");
 	}
 	private void quit()
 	{
