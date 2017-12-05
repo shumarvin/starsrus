df3dda577ffe513ffe114daeceab6f781e9d30d0
Marvin Shu <marvinshu@umail.ucsb.edu>
2017-12-05 12:18:00 -0800

    user interface for addInterest complete

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -282,7 +282,16 @@ public class UserInterface
 	private void showAddInterest()
 	{
 		System.out.println("Adding interest....");
-		dbAdapter.addInterest();
+		if(dbAdapter.addInterest())
+		{
+			System.out.println("Successfully added interest to all accounts.");
+			System.out.println();
+		}
+		else
+		{
+			System.out.println("Error occurred. Please see above for details.");
+			System.out.println();	
+		}
 	}
 	private void showMonthlyStatement()
 	{
